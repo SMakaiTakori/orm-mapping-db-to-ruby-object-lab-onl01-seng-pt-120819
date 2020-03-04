ab1a8d59c6de09d9d97a4af56f2d2bf4dd45c4d0
S. Makai Takori <makait87@gmail.com>
2020-03-04 18:22:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,7 +71,9 @@ class Student
       LIMIT ?
     SQL
     
-    DB[:conn].execute(sql, x)
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end
   end
     
   def save
